8ae0a6323b0b70b2527039915ababe0d003a4aef
Brandon Hamilton <brandon.hamilton@gmail.com>
2016-07-17 23:58:21 +0200

    Support for Socket and Channel operations

--- src/Phoenix.js
+++ src/Phoenix.js
@@ -1,7 +1,9 @@
 /* global exports */
-exports.newSocket = function(endpoint) {
-  return function() {
-    return new window.Phoenix.Socket(endpoint);
+exports.newSocketImpl = function(endpoint) {
+  return function(options) {
+    return function() {
+      return new window.Phoenix.Socket(endpoint, options);
+    }
   }
 }
 
@@ -11,10 +13,86 @@ exports.connect = function(socket) {
   }
 }
 
+exports.disconnect = function(socket) {
+  return function(callback) {
+    return function(code) {
+      return function(reason) {
+        return function() {
+          socket.disconnect(callback(socket), code, reason);
+        }
+      }
+    }
+  }
+}
+
+exports.sendHeartbeat = function(socket) {
+  return function() {
+    socket.sendHeartbeat();
+  }
+}
+
+exports.flushSendBuffer = function(socket) {
+  return function() {
+    socket.flushSendBuffer();
+  }
+}
+
+exports.isConnected = function(socket) {
+  return function() {
+    return socket.isConnected();
+  }
+}
+
+exports.connectionStateImpl = function(socket) {
+  return function() {
+    return socket.connectionState();
+  }
+}
+
+exports.protocol = function(socket) {
+  return function() {
+    return socket.protocol();
+  }
+}
+
+exports.endPointURL = function(socket) {
+  return function() {
+    return socket.endPointURL();
+  }
+}
+
+function registerCallback0(_this, func) {
+  return function(callback) {
+    return function() {
+      return _this[func](callback(_this));
+    }
+  }
+}
+
+function registerCallback1(_this, func) {
+  return function(callback) {
+    return function() {
+      return _this[func](function(arg) {
+        return callback(_this)(arg)(); 
+      });
+    }
+  }
+}
+
+exports.onOpen         = function(socket)  { return registerCallback0(socket,  'onOpen') };
+exports.onMessage      = function(socket)  { return registerCallback1(socket,  'onMessage') };
+
+exports.onSocketClose  = function(socket)  { return registerCallback1(socket,  'onClose') };
+exports.onSocketError  = function(socket)  { return registerCallback1(socket,  'onError') };
+exports.onChannelClose = function(channel) { return registerCallback1(channel, 'onClose') };
+exports.onChannelError = function(channel) { return registerCallback1(channel, 'onError') };
+
 exports.channel = function(socket) {
   return function(topic) {
-    return function() {
-      return socket.channel(topic);
+    return function(params) {
+      return function() {
+        return socket.channel(topic);
+      }
     }
   }
 }
@@ -27,9 +105,21 @@ exports.remove = function(socket) {
   }
 }
 
+exports.makeRef = function(socket) {
+  return function() {
+    return socket.makeRef();
+  }
+}
+
 exports.join = function(channel) {
   return function() {
     return channel.join();
+  }
+}
+
+exports.rejoinUntilConnected = function(channel) {
+  return function() {
+    return channel.rejoinUntilConnected();
   }
 }
 
@@ -39,10 +129,59 @@ exports.leave = function(channel) {
   }
 }
 
+exports.canPush = function(channel) {
+  return function() {
+    return channel.canPush();
+  }
+}
+
 exports.push = function(channel) {
   return function(event) {
     return function(payload) {
-      return channel.push(event, payload);
+      return function() {
+        return channel.push(event, payload);
+      }
     }
   }
 }
+
+exports.on = function(channel) {
+  return function(event) {
+    return function(callback) {
+      return function() {
+        return channel.on(event, function(arg) {
+          callback(channel)(event)(arg)(); 
+        });
+      }
+    }
+  }
+}
+
+exports.off = function(channel) {
+  return function(event) {
+    return function() {
+      return channel.off(event);
+    }
+  }
+}
+
+exports.send = function(push) {
+  return function() {
+    return push.send();
+  }
+}
+
+exports.receive = function(push) {
+  return function(status) {
+    return function(callback) {
+      return function() {
+        return push.receive(status, function() {
+          var cb = callback(push);
+          [].slice.call(arguments).forEach(function (arg) { cb = cb(arg); });
+          return cb();
+        });
+      }
+    }
+  }
+}
+
--- src/Phoenix.purs
+++ src/Phoenix.purs
@@ -3,23 +3,61 @@ module Phoenix
     PHOENIX()
   , Socket()
   , Channel()
-
+  , Push()
+  , ConnectionState(SocketConnecting,SocketOpen,SocketClosing,SocketClosed)
   , Endpoint()
   , Topic()
   , Event()
+  , Status()
+  , Message()
+  
+  , defaultSocketOptions
+  , timeout
+  , heartbeatIntervalMs
+  , reconnectAfterMs
+  , params
 
   , newSocket
   , connect
+  , disconnect
   , channel
   , remove
+  , sendHeartbeat
+  , flushSendBuffer
+  , makeRef
+
+  , protocol
+  , endPointURL
+  , isConnected
+  , connectionState
+
+  , onOpen
+  , onMessage
+  , onSocketError
+  , onSocketClose
+
   , join
-  , leave
   , push
+  , leave
+
+  , rejoinUntilConnected
+  , canPush
+
+  , onChannelError
+  , onChannelClose
+  , on
+  , off
+
+  , send
+  , receive
 
   ) where
 
-import Prelude
+import Prelude (Unit, (<$>))
 import Control.Monad.Eff (Eff)
+import Data.Monoid (mempty)
+import Data.Foreign (Foreign())
+import Data.Options (Options, Option, opt, options)
 
 -- | The `Phoenix` effect denotes computations which read/write from/to the Phoenix Channels.
 foreign import data PHOENIX :: !
@@ -30,34 +68,97 @@ foreign import data Socket :: *
 -- | A Phoenix Channel
 foreign import data Channel :: *
 
--- | Phoenix LongPoll Transport
-foreign import data LongPoll :: *
+-- | A Phoenix Push object
+foreign import data Push :: *
 
--- | Phoenix Ajax Transport
-foreign import data Ajax :: *
-
--- | Phoenix Presence
-foreign import data Presence :: *
-
-data ConnectionState = Connecting | Open | Closing | Closed
+data ConnectionState = SocketConnecting | SocketOpen | SocketClosing | SocketClosed
 
 -- | An Endpoint URL is just a string
 type Endpoint = String
 
 type Topic = String
 
-newtype Event = Event String
+type Event = String
 
-foreign import newSocket :: forall eff. Endpoint -> Eff (phoenix :: PHOENIX | eff) Socket
+type Status = String
 
+type Message = {
+  topic   :: Topic,
+  event   :: Event,
+  payload :: Foreign,
+  ref     :: String
+}
+
+-- | Socket options
+defaultSocketOptions :: Options Socket
+defaultSocketOptions = mempty
+
+timeout :: Option Socket Int
+timeout = opt "timeout"
+
+heartbeatIntervalMs :: Option Socket Int
+heartbeatIntervalMs = opt "heartbeatIntervalMs"
+
+reconnectAfterMs :: Option Socket (Int -> Int)
+reconnectAfterMs = opt "reconnectAfterMs"
+
+params :: Option Socket Foreign
+params = opt "params"
+
+-- | Create a new Phoenix Socket
+newSocket :: forall eff. Endpoint -> Options Socket -> Eff (phoenix :: PHOENIX | eff) Socket
+newSocket e o = newSocketImpl e (options o)
+
+foreign import newSocketImpl :: forall eff. Endpoint -> Foreign -> Eff (phoenix :: PHOENIX | eff) Socket
+
+-- | Socket operations
 foreign import connect :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import disconnect :: forall eff a. Socket -> (Socket -> Eff (phoenix :: PHOENIX | eff) a) -> Int -> String -> Eff (phoenix :: PHOENIX | eff) Unit
 
-foreign import channel :: forall eff. Socket -> Topic ->  Eff (phoenix :: PHOENIX | eff) Channel
-
+foreign import channel :: forall eff. Socket -> Topic -> Foreign -> Eff (phoenix :: PHOENIX | eff) Channel
 foreign import remove :: forall eff. Socket -> Channel -> Eff (phoenix :: PHOENIX | eff) Unit
 
-foreign import join :: forall eff. Channel -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import sendHeartbeat :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import flushSendBuffer :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import makeRef :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) String
 
-foreign import leave :: forall eff. Channel -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import protocol :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) String
+foreign import endPointURL :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) String
+foreign import isConnected :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) Boolean
 
-foreign import push :: forall eff. Channel -> Event -> Eff (phoenix :: PHOENIX | eff) Unit
+connectionState :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) ConnectionState
+connectionState s = cs <$> connectionStateImpl s
+  where
+    cs :: String -> ConnectionState
+    cs "connecting" = SocketConnecting
+    cs "open"       = SocketOpen
+    cs "closing"    = SocketClosing
+    cs _            = SocketClosed
+
+foreign import connectionStateImpl :: forall eff. Socket -> Eff (phoenix :: PHOENIX | eff) String
+
+-- | Register callbacks
+foreign import onOpen        :: forall eff a. Socket -> (Socket -> Eff (phoenix :: PHOENIX | eff) a) -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import onMessage     :: forall eff a. Socket -> (Socket -> Message -> Eff (phoenix :: PHOENIX | eff) a) -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import onSocketClose :: forall eff a. Socket -> (Socket -> String -> Eff (phoenix :: PHOENIX | eff) a) -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import onSocketError :: forall eff a. Socket -> (Socket -> String -> Eff (phoenix :: PHOENIX | eff) a) -> Eff (phoenix :: PHOENIX | eff) Unit
+
+-- | Channel operations
+foreign import join :: forall eff. Channel -> Eff (phoenix :: PHOENIX | eff) Push
+foreign import push :: forall eff. Channel -> Event -> Foreign -> Eff (phoenix :: PHOENIX | eff) Push
+foreign import leave :: forall eff. Channel -> Eff (phoenix :: PHOENIX | eff) Push
+
+foreign import rejoinUntilConnected  :: forall eff. Channel -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import canPush :: forall eff. Channel -> Eff (phoenix :: PHOENIX | eff) Boolean
+
+-- | Register callbacks
+foreign import onChannelClose :: forall eff a. Channel -> (Channel -> Event -> Eff (phoenix :: PHOENIX | eff) a) -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import onChannelError :: forall eff a. Channel -> (Channel -> String -> Eff (phoenix :: PHOENIX | eff) a) -> Eff (phoenix :: PHOENIX | eff) Unit
+
+-- | Listen for messages on a channel
+foreign import on  :: forall eff a. Channel -> Event -> (Channel -> Event -> Foreign -> Eff (phoenix :: PHOENIX | eff) a) -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import off :: forall eff. Channel -> Event -> Eff (phoenix :: PHOENIX | eff) Unit
+
+-- | Push operations
+foreign import send    :: forall eff. Push -> Eff (phoenix :: PHOENIX | eff) Unit
+foreign import receive :: forall eff a. Push -> Status -> (Push -> Foreign -> Eff (phoenix :: PHOENIX | eff) a) -> Eff (phoenix :: PHOENIX | eff) Push
